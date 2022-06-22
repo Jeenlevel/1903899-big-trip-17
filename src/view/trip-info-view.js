1a3abcdc5e5b8ba32c7c4624827f4fe1ba59ad30
Jeenlevel <inlevel94@gmail.com>
2022-06-22 19:25:25 +0300

    final

--- src/view/trip-info-view.js
+++ src/view/trip-info-view.js
@@ -2,65 +2,65 @@ import dayjs from 'dayjs';
 import { DateFormat } from '../consts.js';
 import AbstractView from '../framework/view/abstract-view.js';
 
-const getTripTitle = (waypoints) => {
-  switch (waypoints.length) {
+const getTripTitle = (points) => {
+  switch (points.length) {
     case 1:
-      return [waypoints[0].destination.name];
+      return [points[0].destination.name];
     case 2:
-      return [`${waypoints[0].destination.name} &mdash; ${waypoints[1].destination.name}`];
+      return [`${points[0].destination.name} &mdash; ${points[1].destination.name}`];
     case 3:
-      return [`${waypoints[0].destination.name} &mdash; ${waypoints[1].destination.name} &mdash; ${waypoints[2].destination.name}`];
+      return [`${points[0].destination.name} &mdash; ${points[1].destination.name} &mdash; ${points[2].destination.name}`];
     default:
-      return [`${waypoints[0].destination.name} &mdash; ... &mdash; ${waypoints[waypoints.length - 1].destination.name}`];
+      return [`${points[0].destination.name} &mdash; ... &mdash; ${points[points.length - 1].destination.name}`];
   }
 };
 
-const getTripDates = (waypoints) => `${dayjs(waypoints[0].dateFrom).format(DateFormat.DAY_MONTH)}&nbsp;&mdash;&nbsp${dayjs(waypoints[waypoints.length - 1].dateTo).format(DateFormat.DAY_MONTH)}`;
+const getTripDates = (points) => `${dayjs(points[0].dateFrom).format(DateFormat.DAY_MONTH)}&nbsp;&mdash;&nbsp${dayjs(points[points.length - 1].dateTo).format(DateFormat.DAY_MONTH)}`;
 
-const getOffersCost = (waypoints, offersData) => {
-  const waypointsOffersPrice = [];
+const getOffersCost = (points, offersData) => {
+  const pointsOffersPrice = [];
 
-  for(const waypoint of waypoints) {
+  for(const waypoint of points) {
     const offerIndex = offersData.findIndex((item) => item.type === waypoint.type);
     const waypointOffers = offersData[offerIndex].offers;
     const targetOffers = waypointOffers.filter((item) => waypoint.offers.some((el) => item.id === el));
-    targetOffers.forEach((item) => waypointsOffersPrice.push(item.price));
+    targetOffers.forEach((item) => pointsOffersPrice.push(item.price));
   }
 
-  const offersCost = waypointsOffersPrice.reduce((acc, price) => acc + price, 0);
+  const offersCost = pointsOffersPrice.reduce((acc, price) => acc + price, 0);
   return offersCost;
 };
 
-const getTripCost = (waypoints, offers) => {
-  const basePriceCosts = waypoints.reduce((sum, waypoint) => sum + Number(waypoint.basePrice), 0);
-  const offersCost = getOffersCost(waypoints, offers);
+const getTripCost = (points, offers) => {
+  const basePriceCosts = points.reduce((sum, waypoint) => sum + Number(waypoint.basePrice), 0);
+  const offersCost = getOffersCost(points, offers);
 
   return basePriceCosts + offersCost;
 };
 
-const createTripInfoTemplate = (waypoints, offers) => (
+const createTripInfoTemplate = (points, offers) => (
   `<section class="trip-main__trip-info  trip-info">
     <div class="trip-info__main">
-      <h1 class="trip-info__title">${getTripTitle(waypoints)}</h1>
-      <p class="trip-info__dates">${getTripDates(waypoints)}</p>
+      <h1 class="trip-info__title">${getTripTitle(points)}</h1>
+      <p class="trip-info__dates">${getTripDates(points)}</p>
     </div>
     <p class="trip-info__cost">
-      Total: &euro;&nbsp;<span class="trip-info__cost-value">${getTripCost(waypoints, offers)}</span>
+      Total: &euro;&nbsp;<span class="trip-info__cost-value">${getTripCost(points, offers)}</span>
     </p>
   </section>`
 );
 
 export default class TripInfoView extends AbstractView {
-  #waypoints = null;
+  #points = null;
   #offers = null;
 
-  constructor(waypoints, offers) {
+  constructor(points, offers) {
     super();
-    this.#waypoints = waypoints;
+    this.#points = points;
     this.#offers = offers;
   }
 
   get template() {
-    return createTripInfoTemplate(this.#waypoints, this.#offers);
+    return createTripInfoTemplate(this.#points, this.#offers);
   }
 }
